6feb72b8ea98546f512fc29e7cee2b353ef01718
seungjin-kim <seungjink1@gmail.com>
2019-12-27 20:22:57 -0800

    Add basic css styling for array bar and add button to generate new array

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -16,8 +16,8 @@ export default class SortingVisualizer extends React.Component {
 
   resetArray() {
     const array = [];
-    for (let i = 0; i < 100; i++) {
-      array.push(randomIntFromInterval(5, 1000));
+    for (let i = 0; i < 320; i++) {
+      array.push(randomIntFromInterval(5, 820));
     }
     this.setState({array});
   }
@@ -26,13 +26,15 @@ export default class SortingVisualizer extends React.Component {
     const {array} = this.state;
 
     return (
-      <>
+      <div className="array-container">
         {array.map((value, idx) => (
-          <div className="array-bar" key={idx}>
-            {value}
-          </div>
+          <div 
+            className="array-bar"
+            key={idx}
+            style={{height: `${value}px`}}></div>
         ))}
-      </>
+        <button onClick={() => this.resetArray()}>Reset Array</button>
+      </div>
     );
   }
 }
