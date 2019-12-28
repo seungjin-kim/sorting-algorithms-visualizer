72b7d9664cfd4f2976f00ed618b6aec7262ea0fc
seungjin-kim <seungjink1@gmail.com>
2019-12-27 18:16:15 -0800

    Initialize folder directory and SortingVisualizer class

---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SortingVisualizer.css';
+
+export default class SortingVisualizer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      array: [],
+    };
+  }
+
+  
+}
