94346c90c9817af6b6cecb46be526ab87dc84777
seungjin-kim <seungjink1@gmail.com>
2019-12-27 18:41:41 -0800

    Add function for generating random integer

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -26,9 +26,19 @@ export default class SortingVisualizer extends React.Component {
     const {array} = this.state;
 
     return (
-      
+      <>
+        {array.map((value, idx) => (
+          <div className="array-bar" key={idx}>
+            {value}
+          </div>
+        ))}
+      </>
     );
   }
+}
 
-
+// https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
