425120243513685e8d9a2ad63e4e61a77b5a2334
seungjin-kim <seungjink1@gmail.com>
2019-12-28 18:29:00 -0800

    Implemented main mergesort function

--- src/Algorithms/algorithms.js
+++ src/Algorithms/algorithms.js
@@ -1,4 +1,8 @@
-export const mergeSort = array => {
-  if (array.length === 1) return array;
-  
-};
+export function mergeSort(array) {
+  const animations = [];
+  if (array.length <= 1) return array;
+  const auxArray = array.slice();
+  msHelper(array, auxArray, 0, array.length - 1, animations);
+  return animations;
+}
+
