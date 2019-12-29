b95e4dede820306a3c0f1e39a33622f0039c9045
seungjin-kim <seungjink1@gmail.com>
2019-12-28 19:31:31 -0800

    mergeArrays functions to be used in main mergeSort function

--- src/Algorithms/algorithms.js
+++ src/Algorithms/algorithms.js
@@ -15,5 +15,33 @@ function msHelper(mainArray, auxArray, startIdx, endIdx, animations) {
 }
 
 function mergeArrays(mainArray, auxArray, startIdx, midIdx, endIdx, animations) {
-  
+  let k = startIdx;
+  let i = startIdx;
+  let j = midIdx + 1;
+  while (i <= midIdx && j <= endIdx) {
+    // i and j are the values being compared - their colors need to be changed
+    animations.push([i, j])
+    // push them again to revert their color
+    animations.push([i, j])
+    if (auxArray[i] <= auxArray[j]) {
+      animations.push([k, auxArray[i]]);
+      mainArray[k++] = auxArray[i++];
+    } else {
+      animations.push([k, auxArray[j]]);
+      mainArray[k++] = auxArray[j++];
+    }
+  }
+  while (i <= midIdx) {
+    animations.push([i, i])
+    animations.push([i, i])
+    animations.push([k, auxArray[i]])
+    mainArray[k++] = auxArray[i++];
+  }
+  while (j <= endIdx) {
+    animations.push([j, j]);
+    animations.push([j, j]);
+    animations.push([k, auxArray[j]])
+    mainArray[k++] = auxArray[j++];
+  }
 }
+
